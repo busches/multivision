4c37ac17fc05bf0df45d1676682beb49467b6c5f
Scott Busche <swbusche@gmail.com>
2014-08-12 21:56:35 -0500

    Configure was removed in Express 4

--- server.js
+++ server.js
@@ -3,10 +3,8 @@
  var env = process.env.NODE_EVN  = process.env.NODE_EVN || 'development';
 
 var app = express();
-app.configure(function() {
-	app.set('views', __dirname + '/server/views');
-	app.set('view_engine', 'jade');
-});
+app.set('views', __dirname + '/server/views');
+app.set('view_engine', 'jade');
 
 app.get('*', function(request, response) {
 	response.render('index');
