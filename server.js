65e475e3601a4f900facda30ce133965663510b9
Scott Busche <swbusche@gmail.com>
2014-08-12 21:52:40 -0500

    Add favicon and listen for requests

--- server.js
+++ server.js
@@ -3,11 +3,15 @@
  var env = process.env.NODE_EVN  = process.env.NODE_EVN || 'development';
 
 var app = express();
- app.configure(function() {
- 	app.set('views', __dirname + '/server/views');
- 	app.set('view_engine', 'jade');
- });
+app.configure(function() {
+	app.set('views', __dirname + '/server/views');
+	app.set('view_engine', 'jade');
+});
 
- app.get('*', function(request, response) {
- 	response.render('index');
- });
+app.get('*', function(request, response) {
+	response.render('index');
+});
+
+var port = 3030;
+app.listen(port);
+console.log('Listening on port ' + port + '...');
