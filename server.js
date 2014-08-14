91709c9cad9e445c911b50da511015679006e66d
Scott Busche <swbusche@gmail.com>
2014-08-13 20:51:06 -0500

    Add stylus and serve static files

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var express = require('express');
+var stylus = require('stylus');
 
-
-var env = process.env.NODE_EVN  = process.env.NODE_EVN || 'development';
+var env = process.env.NODE_EVN = process.env.NODE_EVN || 'development';
 
 var app = express();
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(stylus.middleware({
+	src: __dirname + '/public',
+	compile: compile
+}));
+app.use(express.static(__dirname + '/public'));
+
+function compile(string, path) {
+	return stylus(string).set('filename', path);
+}
 
 app.get('*', function(request, response) {
 	response.render('index');
