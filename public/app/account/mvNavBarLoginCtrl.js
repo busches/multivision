4afd72d9a10cc1ac5734dc97edc2ebd3e7c5650d
Scott Busche <swbusche@gmail.com>
2014-08-28 21:08:09 -0500

    Remove extra console.log

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,11 +1,9 @@
 angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http) {
 	$scope.signin = function(username, password) {
-
 		$http.post('/login', {
 			username: username,
 			password: password
 		}).then(function(response) {
-			console.log(response);
 			if (response.data.success) {
 				console.log('logged in!');
 			} else {
