3f40c6ac3df0d71e72ab0187c0e966ba495b0184
Scott Busche <swbusche@gmail.com>
2014-09-01 20:37:55 -0500

    Refactor server auth out of routes into own file

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,32 +1,11 @@
-var passport = require('passport');
+var auth = require('./auth');
 
 module.exports = function(app) {
 	app.get('/partials/*', function(request, response) {
 		response.render('../../public/app/' + request.params[0]);
 	});
 
-	app.post('/login', function(request, response, next) {
-		var auth = passport.authenticate('local', function(error, user) {
-			if (error) {
-				return next(error);
-			}
-			if (!user) {
-				response.send({
-					success: false
-				});
-			}
-			request.logIn(user, function(error) {
-				if (error) {
-					return next(error);
-				}
-				response.send({
-					success: true,
-					user: user
-				});
-			});
-		});
-		auth(request, response, next);
-	});
+	app.post('/login', auth.authenticate);
 
 	app.get('*', function(request, response) {
 		response.render('index', {});
