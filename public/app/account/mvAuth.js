21ffec8f3315867d3aa05ff390b22ccce9d54d90
Scott Busche <swbusche@gmail.com>
2014-09-07 11:07:08 -0500

    Add createUser function to mvAuth

--- public/app/account/mvAuth.js
+++ public/app/account/mvAuth.js
@@ -1,39 +1,53 @@
- angular.module('app').factory('mvAuth', function($http, mvIdentity, mvUser, $q) {
- 	return {
- 		authenticateUser: function(username, password) {
- 			var deferred = $q.defer();
- 			$http.post('/login', {
- 				username: username,
- 				password: password
- 			}).then(function(response) {
- 				if (response.data.success) {
- 					var user = new mvUser();
- 					angular.extend(user, response.data.user);
- 					mvIdentity.currentUser = user;
- 					deferred.resolve(true);
- 				} else {
- 					deferred.resolve(false);
- 				}
- 			});
- 			return deferred.promise;
- 		},
- 		logoutUser: function() {
- 			var deferred = $q.defer();
- 			$http.post('/logout', {
- 				logout: true
- 			}).then(function() {
- 				mvIdentity.currentUser = undefined;
- 				deferred.resolve();
- 			});
+'use strict';
 
- 			return deferred.promise;
- 		},
- 		authorizeCurrentUserForRoute: function(role) {
- 			if (mvIdentity.isAuthorized(role)) {
- 				return true;
- 			} else {
- 				return $q.reject('not authorized');
- 			}
- 		}
- 	};
- });
+angular.module('app').factory('mvAuth', function($http, mvIdentity, mvUser, $q) {
+	return {
+		authenticateUser: function(username, password) {
+			var deferred = $q.defer();
+			$http.post('/login', {
+				username: username,
+				password: password
+			}).then(function(response) {
+				if (response.data.success) {
+					var user = new mvUser();
+					angular.extend(user, response.data.user);
+					mvIdentity.currentUser = user;
+					deferred.resolve(true);
+				} else {
+					deferred.resolve(false);
+				}
+			});
+			return deferred.promise;
+		},
+		createUser: function(newUserData) {
+			var newUser = new mvUser(newUserData);
+			var deferred = $q.defer();
+			newUser.$save().then(function() {
+				mvIdentity.currentUser = newUser;
+				deferred.resolve();
+			}, function(response) {
+				deferred.reject(response.data.reason);
+			});
+
+			return deferred;
+		},
+		logoutUser: function() {
+			var deferred = $q.defer();
+			$http.post('/logout', {
+				logout: true
+			}).then(function() {
+				mvIdentity.currentUser = undefined;
+				deferred.resolve();
+			});
+
+			return deferred.promise;
+		},
+		authorizeCurrentUserForRoute: function(role) {
+			if (mvIdentity.isAuthorized(role)) {
+				return true;
+			} else {
+				return $q.reject('not authorized');
+			}
+		}
+	};
+});
