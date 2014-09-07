1b38e843dcac6b026e784b7f2d5fc8baeeac2f08
Scott Busche <swbusche@gmail.com>
2014-09-07 10:44:39 -0500

    Add error function to mvNotifier

--- public/app/common/mvNotifier.js
+++ public/app/common/mvNotifier.js
@@ -1,7 +1,13 @@
+'use strict';
+
 angular.module('app').value('mvToastr', toastr);
 
 angular.module('app').factory('mvNotifier', function(mvToastr) {
 	return {
+		error: function(msg) {
+			mvToastr.error(msg);
+			console.log(msg);
+		},
 		notify: function(msg) {
 			mvToastr.success(msg);
 			console.log(msg);
