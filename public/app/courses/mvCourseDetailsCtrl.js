c610f701cdc0e0924409080d74810bd5cc05b445
Scott Busche <swbusche@gmail.com>
2014-09-14 16:57:36 -0500

    Use our cached course list to get details
    Rendering the server API a bit useless, oh well :)

--- public/app/courses/mvCourseDetailsCtrl.js
+++ public/app/courses/mvCourseDetailsCtrl.js
@@ -1,7 +1,12 @@
-angular.module('app').controller('mvCourseDetailsCtrl', ['mvCourse', '$routeParams', '$scope',
-	function(mvCourse, $routeParams, $scope) {
-		$scope.course = mvCourse.get({
-			_id: $routeParams.id
+angular.module('app').controller('mvCourseDetailsCtrl', ['mvCachedCourses', '$routeParams', '$scope',
+	function(mvCachedCourses, $routeParams, $scope) {
+		mvCachedCourses.query().$promise.then(function(collection) {
+			collection.some(function(course) {
+				if (course._id === $routeParams.id) {
+					$scope.course = course;
+					return true;
+				}
+			});
 		});
 	}
 ]);
